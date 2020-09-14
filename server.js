71895fd3979da68f3ede009a2d9318b7c26bf5de
admin <admin@admins-iMac.local>
2020-09-14 13:34:09 -0700

    phase 2

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer(function(req, res) {
+    if (req.url === '/OK'){
+    console.log("Inbound 'OK' request being processed...");
+    res.writeHead(200);
     res.end();
+    } else {
+        res.writeHead(404);
+        console.log("404 Error");
+        res.end();
+    }
 }).listen(3000, function(){
     console.log("Listening for requests in port 3000");
 });
